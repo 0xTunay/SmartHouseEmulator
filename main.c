4a960f35f61bdabf5e60df65081fcf6154fd123d
0xTunay <tigrus232323k@gmail.com>
2025-05-18 19:04:27 +0300

    add start program

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+/*!
+ * @details main fuction for start program
+ */
+int main(void) {
+
+	puts("Welcome to Smart House Emulator");
+	puts("In this application you can build you own smart system ");
+
+
+	return 0;
+}
