14f3cef9f0b9072efb692dd9fe07c9307533c448
0xTunay <tigrus232323k@gmail.com>
2025-05-23 19:53:32 +0300

    add memroy setting for struct

--- core/SmartHouseCore.c
+++ core/SmartHouseCore.c
@@ -7,22 +7,33 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "../include/SmartHouseCore.h"
 
 /*!
  * @details init smarthouse struct
  */
-void SmartHouseCore_Init(){
-    SmartHouse house;
 
-    SmartHouseCore_Init(&house);
-    house.devices[0].type = DEVICE_LIGHT;
-    house.devices[0].status = STATUS_OFF;
-    house.devices[0].data.temperature = 18;
+void SmartHouseCore_Init(SmartHouse *house){
+
+    if(house == NULL){
+        perror("pointer error");
+        exit(EXIT_FAILURE);
+    }
+
+    house->device_count = 0;
+
+    house->devices = malloc(sizeof(Device) * MAX_DEVICES);
+    if(house->devices == NULL){
+        fprintf(stderr, "Error allocating memory for device array\n");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(house->devices, 0, sizeof(Device) * MAX_DEVICES);
 
     puts("SmartHouseEmulator v1.0");
 
 
-
+    free(house->devices);
 }
--- main.c
+++ main.c
@@ -1,22 +1,21 @@
 /*!
- * @author Tunay
- * @date 2025 05 19
- * @details application for emulation Smart house, you can add and delete device
+ * @file main.c
+ * @brief main file for init smarthouse.
+ *
+ * @author tunay
+ * @date 2025-05-19
  */
-
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "../include/SmartHouseCore.h"
-
+#include "include/SmartHouseCore.h"
 /*!
  * @details main function for start program
  */
 int main(void) {
-	SmartHouse *house;
+    SmartHouse house;
 	puts("Welcome to Smart House Emulator");
 	puts("In this application you can build you own smart system ");
 
-
-	SmartHouseCore_Init( &house);
+	SmartHouseCore_Init(&house);
 }
